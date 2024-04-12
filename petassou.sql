37cc9573e7a1ec402bb141e57d66656dab97897c
Félicien Pillot <felicien@gnu.org>
2024-04-12 17:05:59 +0200

    Nouvelle sauvegarde bdd

--- petassou.sql
+++ petassou.sql
@@ -1,119 +1,162 @@
--- MariaDB dump 10.19-11.3.2-MariaDB, for Linux (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: petassou
--- ------------------------------------------------------
--- Server version	11.3.2-MariaDB
+-- Hôte : localhost
+-- Généré le : ven. 12 avr. 2024 à 15:04
+-- Version du serveur : 11.3.2-MariaDB
+-- Version de PHP : 8.3.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Categories`
+-- Base de données : `petassou`
 --
 
-DROP TABLE IF EXISTS `Categories`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Categories` (
-  `Categorie` varchar(255) NOT NULL,
-  `Sous-categorie` varchar(255) NOT NULL,
-  `Code` int(11) NOT NULL AUTO_INCREMENT,
-  `Poids` int(11) DEFAULT NULL COMMENT 'en grammes',
-  PRIMARY KEY (`Code`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `Categories`
+-- Structure de la table `categories`
 --
 
-LOCK TABLES `Categories` WRITE;
-/*!40000 ALTER TABLE `Categories` DISABLE KEYS */;
-INSERT INTO `Categories` VALUES
-('Mobilier','Chaise',6,4000),
-('Vêtements','Culotte',7,10);
-/*!40000 ALTER TABLE `Categories` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE `categories` (
+  `id_categorie` int(11) NOT NULL,
+  `nom_categorie` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `Produits`
+-- Structure de la table `produits`
 --
 
-DROP TABLE IF EXISTS `Produits`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Produits` (
-  `Produit` int(11) NOT NULL AUTO_INCREMENT,
-  `Description` varchar(255) DEFAULT NULL,
-  `Code` int(11) NOT NULL,
-  `Coût` int(11) DEFAULT 0 COMMENT 'en euros',
-  `Temps passé` int(11) DEFAULT 0 COMMENT 'en heures',
-  `Vendu` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`Produit`),
-  KEY `Code` (`Code`),
-  CONSTRAINT `Code` FOREIGN KEY (`Code`) REFERENCES `Categories` (`Code`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE `produits` (
+  `id_produit` int(11) NOT NULL,
+  `nom_produit` varchar(150) NOT NULL,
+  `description` text DEFAULT NULL,
+  `date_enregistrement` datetime NOT NULL DEFAULT current_timestamp(),
+  `cout_reparation` float DEFAULT NULL,
+  `temps_passe` time DEFAULT NULL,
+  `vendu` tinyint(4) NOT NULL DEFAULT 0,
+  `id_sous_categorie` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `Produits`
+-- Structure de la table `sous_categories`
 --
 
-LOCK TABLES `Produits` WRITE;
-/*!40000 ALTER TABLE `Produits` DISABLE KEYS */;
-INSERT INTO `Produits` VALUES
-(27,'en dentelle',7,0,0,12),
-(28,'Chaise réparée',6,2,1,13),
-(29,'',6,-1,-1,NULL);
-/*!40000 ALTER TABLE `Produits` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE `sous_categories` (
+  `id_sous_categorie` int(11) NOT NULL,
+  `nom_sous_categorie` varchar(50) NOT NULL,
+  `id_categorie` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `Ventes`
+-- Structure de la table `ventes`
 --
 
-DROP TABLE IF EXISTS `Ventes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Ventes` (
-  `Vente` int(11) NOT NULL AUTO_INCREMENT,
-  `Quantite` int(11) NOT NULL DEFAULT 1,
-  `Date` date NOT NULL DEFAULT current_timestamp(),
-  `Produit` int(11) NOT NULL,
-  `Prix libre` int(11) DEFAULT NULL,
-  PRIMARY KEY (`Vente`),
-  KEY `Produit` (`Produit`),
-  CONSTRAINT `Produit` FOREIGN KEY (`Produit`) REFERENCES `Produits` (`Produit`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE `ventes` (
+  `id_vente` int(11) NOT NULL,
+  `quantite` int(11) NOT NULL DEFAULT 1,
+  `date_vente` datetime NOT NULL DEFAULT current_timestamp(),
+  `prix_libre` float DEFAULT NULL,
+  `id_produit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `Ventes`
+-- Index pour les tables déchargées
 --
 
-LOCK TABLES `Ventes` WRITE;
-/*!40000 ALTER TABLE `Ventes` DISABLE KEYS */;
-INSERT INTO `Ventes` VALUES
-(12,2,'2024-03-25',27,20),
-(13,1,'2024-03-25',28,20);
-/*!40000 ALTER TABLE `Ventes` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- Index pour la table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id_categorie`);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Index pour la table `produits`
+--
+ALTER TABLE `produits`
+  ADD PRIMARY KEY (`id_produit`),
+  ADD KEY `id_categorie` (`id_sous_categorie`);
+
+--
+-- Index pour la table `sous_categories`
+--
+ALTER TABLE `sous_categories`
+  ADD PRIMARY KEY (`id_sous_categorie`),
+  ADD KEY `id_categorie` (`id_categorie`);
+
+--
+-- Index pour la table `ventes`
+--
+ALTER TABLE `ventes`
+  ADD PRIMARY KEY (`id_vente`),
+  ADD KEY `id_produit` (`id_produit`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id_categorie` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `produits`
+--
+ALTER TABLE `produits`
+  MODIFY `id_produit` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `sous_categories`
+--
+ALTER TABLE `sous_categories`
+  MODIFY `id_sous_categorie` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `ventes`
+--
+ALTER TABLE `ventes`
+  MODIFY `id_vente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `produits`
+--
+ALTER TABLE `produits`
+  ADD CONSTRAINT `produits_ibfk_1` FOREIGN KEY (`id_sous_categorie`) REFERENCES `sous_categories` (`id_sous_categorie`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `sous_categories`
+--
+ALTER TABLE `sous_categories`
+  ADD CONSTRAINT `sous_categories_ibfk_1` FOREIGN KEY (`id_categorie`) REFERENCES `categories` (`id_categorie`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `ventes`
+--
+ALTER TABLE `ventes`
+  ADD CONSTRAINT `ventes_ibfk_1` FOREIGN KEY (`id_produit`) REFERENCES `produits` (`id_produit`) ON DELETE NO ACTION ON UPDATE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-03-25 13:25:23
