cb5ef394d0387812a20d5354657c85884645b9a7
Félicien Pillot <felicien@gnu.org>
2024-05-02 16:36:23 +0200

    details/summary pour déplier les catégories

--- views/gestionCat.view.php
+++ views/gestionCat.view.php
@@ -7,36 +7,40 @@ ob_start()
 <u><h2>Gestion des catégories</h2></u>
 
 <a href='index.php?page=addFormCategorie'><button class="btn">Nouvelle catégorie</button></a>
+<div class="multicard">
 
     <?php
     foreach ($tableau as $nomcat => $reponsecat) {
 	$sousCats = $reponsecat->fetchAll();
     ?>
-	<div class="card">
-            <p class="liensboutons">
-		<b><u><?= $nomcat ?></u></b> 
-		<a class="poubelle" href='index.php?page=deleteCategorie&nom_categorie=<?=$nomcat?>'>&#x1F5D1;</a><br>
+	<div class="card liensboutons">
+	    <details>
+		<summary><li></li>
+		    <b><u><?= $nomcat ?></u></b>
+		    <a class="poubelle" href='index.php?page=deleteCategorie&nom_categorie=<?=$nomcat?>'>&#x1F5D1;</a><br>
+		</summary>
+		<ul>
+		    <?php
+		    foreach ($sousCats as $sousCat) {
+		    ?>
+			<li>
+			    <?= $sousCat['nom_sous_categorie'] ?>
+			    (<?= $sousCat['poids'] ?>&nbsp;g)
+			    <a class="poubelle" href='index.php?page=deleteSousCategorie&nom_sous_categorie=<?=$sousCat['nom_sous_categorie']?>'>&#x1F5D1;</a>
+			</li>
+		    <?php
+		    }
+		    ?>
+		</ul>
 		<a  href='index.php?page=addFormCategorie&nom_categorie=<?=$nomcat?>'><button class="btn newSouCat">Nouvelle sous-catégorie</button></a><br>
-		
-		<div class="multicard">
-		<?php
-		foreach ($sousCats as $sousCat) {
-		?>
-		    <div class="card">
-			<?= $sousCat['nom_sous_categorie'] ?>
-			<a class="poubelle" href='index.php?page=deleteSousCategorie&nom_sous_categorie=<?=$sousCat['nom_sous_categorie']?>'>&#x1F5D1;</a>
-			<ul>
-			    <li>(<?= $sousCat['poids'] ?>&nbsp;g)</li>
-			</ul>
-		    </div>
-		<?php
-		}
-		?>
-		</div>
+	    </details>
             </p>
 	</div>
     <?php
     }
+    ?>
+</div>
+<?php
 $content = ob_get_clean();
 require('base.view.php');
 ?>
